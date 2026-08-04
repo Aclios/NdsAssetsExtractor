6a8be5e9dcc6c37b61ee103e73826060bf8b3d22
Aclios <166991832+Aclios@users.noreply.github.com>
2026-08-04 22:19:43 +0200

    feat: add support to YJAJ (theresia - Dear Emile JP)

--- extractors/YJA/README.md
+++ extractors/YJA/README.md
@@ -1,7 +1,9 @@
-# theresia Dear Emile (Aksys Games)
+# theresia -テレジア- Dear Emile (Aksys Games)
 
 Most assets from the game can be exported, but:
 
 - Some bg entries are skipped because the decompressed files (huffman) are not valid NCGR. Might be a decompression issue.
 
-- Scripts are stored in a very annoying way: all the text for a script is concatened in one file with no terminator, meaning offsets and sizes (that are stored in the .adv script files) are required. Also there is no line breaks, each line is a text entry. So while all the text is extracted, it's not very convenient and additional work to reorganize it would be good.
+- Scripts are stored in a very annoying way: all the text for a script is concatened in one file with no terminator, meaning offsets and sizes (that are stored in the .adv script files) are required. Also there is no line breaks, each line is a text entry. So while all the text is extracted, it's not very convenient and additional work to reorganize it would be good. The japanese and NA versions also work a bit differently since the JP version uses common.txt files that contain the concatened strings for several scripts while the NA version has one txt file per script.
+
+Also, there seems a bunch of unused text in the japanese version, between 0x3106d and 0x39d3b in the sub/common.txt file (after decompression). This text is dumped in an unknown_text.txt, however as explained above it's a concatenation of a lot of different entries that can't be automatically split.
--- extractors/YJA/common/theresia_script.py
+++ extractors/YJA/common/theresia_script.py
@@ -14,11 +14,14 @@ TEXT_TYPE_MAP = {
 
 
 class TheresiaScript(File):
+    def __init__(self, filepath: Path, expected_next_offset: int):
+        self.expected_next_offset = expected_next_offset
+        super().__init__(filepath)
+
     def read(self, f: EndianBinaryReader):
         self.entries: list[TheresiaScriptText] = []
         filesize = f.get_size()
         val = f.read_UInt8()
-        expected_next_offset = 0
         # A big hacky way to find all strings in the script
         # Works because luckily offsets are in ascending order
         while f.tell() < filesize - 5:
@@ -27,11 +30,11 @@ class TheresiaScript(File):
                     entry = TheresiaScriptText(f, val)
                     if (
                         entry.size != 0
-                        and entry.offset == expected_next_offset
+                        and entry.offset == self.expected_next_offset
                         and entry.size < 0x80
                     ):
                         self.entries.append(entry)
-                        expected_next_offset += entry.size
+                        self.expected_next_offset += entry.size
                     else:
                         f.seek(-5, 1)
                 else:
@@ -41,11 +44,11 @@ class TheresiaScript(File):
                     entry = TheresiaScriptText(f, val)
                     if (
                         entry.size != 0
-                        and entry.offset == expected_next_offset
+                        and entry.offset == self.expected_next_offset
                         and entry.size < 0x80
                     ):
                         self.entries.append(entry)
-                        expected_next_offset += entry.size
+                        self.expected_next_offset += entry.size
                     else:
                         f.seek(-5, 1)
                 else:
@@ -54,14 +57,8 @@ class TheresiaScript(File):
 
     def export_to_excel(self, out_path: Path, text_data: bytes):
         data = [(entry.type, entry.get_text(text_data)) for entry in self.entries]
-        test = bytes()
         df = pd.DataFrame(data=data, columns=["Type", "Text"])
         df.to_excel(out_path, index=False)
-        # Ensure we got everything
-        for t in df["Text"]:
-            test += t.encode("shift-jis-2004")
-            if test != text_data[: len(test)]:
-                raise Exception("Error: not all strings were found.")
 
 
 class TheresiaScriptText:
