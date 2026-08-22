6b2a2a1ec0b4ac1f82a075f97fb7d1d732929196
Aclios <166991832+Aclios@users.noreply.github.com>
2026-08-22 17:36:49 +0200

    feat: sysmes

---
+++ extractors/BSK/common/text/sysmes.py
@@ -0,0 +1,36 @@
+from utils.excel import write_excel
+from engines.chunsoft import SIR0
+from ndstools.fs import EndianBinaryReader
+from .utils import read_shift_jis_at
+
+
+class SysMes999(SIR0):
+    def _read(self, f: EndianBinaryReader):
+        f.seek(self.info_start)
+        self.entries: list[SysMes999Entry] = []
+        while f.peek(4) != bytes(4):
+            self.entries.append(SysMes999Entry(f))
+
+    def export_excel(self, out_path: str):
+        data = []
+        for entry in self.entries:
+            data.append([entry.symb, ""])
+            data.extend([["", page] for page in entry.pages])
+        columns = ["Symbol", "Text"]
+        write_excel(out_path, data, columns)
+
+
+class SysMes999Entry:
+    def __init__(self, f: EndianBinaryReader):
+        self.symb_offset = f.read_UInt32()
+        self.unk1 = f.read_UInt32()
+        self.unk2 = f.read_UInt32()
+        self.unk3 = f.read_UInt32()
+        self.unk4 = f.read_UInt32()
+        self.table_offset = f.read_UInt32()
+        pos = f.tell()
+        f.seek(self.table_offset)
+        self.pages_offsets = list(iter(lambda: f.read_UInt32(), 0))
+        self.pages = [read_shift_jis_at(f, offset) for offset in self.pages_offsets]
+        self.symb = read_shift_jis_at(f, self.symb_offset)
+        f.seek(pos)
