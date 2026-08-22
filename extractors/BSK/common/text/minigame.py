b7344714524ebd20b5e8254916ab805700df0cbb
Aclios <166991832+Aclios@users.noreply.github.com>
2026-08-22 17:07:22 +0200

    feat: minigame

---
+++ extractors/BSK/common/text/minigame.py
@@ -0,0 +1,30 @@
+from utils.excel import write_excel
+from engines.chunsoft import SIR0
+from ndstools.fs import EndianBinaryReader
+from .utils import read_shift_jis_at
+
+
+class Minigame999(SIR0):
+    def _read(self, f: EndianBinaryReader):
+        f.seek(self.info_start)
+        self.symb_offset = f.read_UInt32()
+        self.table_start = f.read_UInt32()
+        f.seek(self.table_start)
+        self.entries: list[Minigame999Entry] = []
+        while f.peek(4) != bytes(4):
+            self.entries.append(Minigame999Entry(f))
+
+    def export_excel(self, out_path: str):
+        data = [[entry.symb, entry.text] for entry in self.entries]
+        columns = ["Symbol", "Text"]
+        write_excel(out_path, data, columns)
+
+
+class Minigame999Entry:
+    def __init__(self, f: EndianBinaryReader):
+        self.symb_offset = f.read_UInt32()
+        self.text_offset = f.read_UInt32()
+        pos = f.tell()
+        self.symb = read_shift_jis_at(f, self.symb_offset)
+        self.text = read_shift_jis_at(f, self.text_offset)
+        f.seek(pos)
